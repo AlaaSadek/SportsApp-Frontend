e42b8457a9447da6e1da4280a79b0650bed9b758
Mai Sherif <mai.sherif1997@hotmail.com>
2020-05-06 00:42:28 +0200

    Add annoucementItem and announcementSection

---
+++ src/components/global/announcementItem.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, StyleSheet, Text, Dimensions,ImageBackground,TouchableOpacity } from 'react-native'
+
+const AnnouncementlItem = ({ item },props) => {
+    let containerStyle = styles.container;
+    if (props.style) containerStyle = { ...containerStyle, ...props.style };
+    return (
+        <View style={containerStyle}>
+            <TouchableOpacity>
+            <ImageBackground style={styles.image} source={{ uri: item.url }} >
+                <Text style={styles.date}> {item.date}</Text>
+                <Text style={styles.offer}>{item.offer}</Text>
+          </ImageBackground>
+          </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 172, 
+        width: Dimensions.get('window').width*0.73, 
+        marginLeft: Dimensions.get('window').width*0.04, 
+        borderRadius:10,
+        backgroundColor:'#050000',
+        flexGrow:1
+    },
+   
+    textView: {
+        position: 'absolute',
+        bottom: 10,
+        margin: 10,
+        left: 5,
+    },
+    image: {
+        resizeMode:'center',
+        width:'100%',
+        height:'100%',
+        borderRadius:10,
+        overflow: 'hidden',
+        opacity: 0.59,
+    },
+    date:{
+        fontSize:15,
+        color:'#FFFFFF',
+        fontFamily:'Montserrat_Bold',
+        textAlign:'center',
+        marginTop:'40%'
+    },
+    offer:
+    {
+        fontSize:20,
+        color:'#D8365D',
+        fontFamily:'Montserrat_Bold',
+        textAlign:'center',
+    },
+})
+
+export default AnnouncementlItem
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View,StyleSheet,TouchableOpacity } from "react-native";
 import { clearToken } from "../utils/LocalStorage";
 import MainButton from "../components/global/MainButton";
+import Announcements from "../components/global/Announcements";
 
 const MainScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Main Screen</Text>
 
+  return (
+    <View style={styles.container}>
+      <Text>Main Screen</Text>
+       
       <MainButton
         onPress={() => {
           clearToken();
@@ -16,8 +18,61 @@ const MainScreen = ({ navigation }) => {
       >
         LogOut
       </MainButton>
+  
+    <View >
+    <Text style={styles.homeText}>Home</Text>
+    <View style={styles.announcement_viewallcontainer}>
+        <Text style={styles.announcementText}>Announcements</Text>
+        <TouchableOpacity><Text style={styles.viewallTest}>View All</Text></TouchableOpacity>
+        
     </View>
+     {/* <Announcements data={announcementData}/> */}
+     <Announcements />
+    
+</View>
+</View>
+
   );
 };
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#FFFFFF'
+    },
+  homeText:
+  {
+      fontSize:35,
+      fontFamily:'Montserrat_Bold',
+      color:'#494EAD',
+      marginLeft:'5%',
+      marginBottom:'5%'
+  },
+  card:{
+      // justifyContent:'center',
+      // alignItems:'center'
+  },
+  announcement_viewallcontainer:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '90%',
+      alignSelf: 'center', 
+     
+  },
+  announcementText:{
+      fontSize:15,
+      fontFamily:'Montserrat_Bold',
+      color:'#030303',
+      marginBottom:'5%'
+  },
+  viewallTest:{
+      fontSize:15,
+      fontFamily:'Montserrat_SemiBold',
+      color:'#D8365D',
+      marginBottom:'5%' 
+  },
+  anouncemtScroll:{
+      marginTop:'5%'
+  }
+})
 
 export default MainScreen;
