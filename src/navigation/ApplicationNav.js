790b4be7285da2e1e66ce0776c3e33c19aca8186
Alaa Sadek <alaa.sa.mahmoud@gmail.com>
2020-05-05 17:11:41 +0200

    added announcement cards

--- src/navigation/ApplicationNav.js
+++ src/navigation/ApplicationNav.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from "react-navigation-stack";
 import MainScreen from "../screens/MainScreen";
+import AllAnnouncements from "../screens/AllAnnouncements";
 
 export default ApplicationNav = createStackNavigator(
   {
     MainScreen,
+    AllAnnouncements,
   },
 
   {
