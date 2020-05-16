c6089b7db23815747dca702e4b6cb5bd9ad4b609
Aya Eissa <ayaahmedeissa@gmail.com>
2020-05-16 20:32:33 +0200

    Fix card details component style on android

--- src/components/global/DetailsCard.js
+++ src/components/global/DetailsCard.js
@@ -1,37 +1,37 @@
 
 import React from "react";
-import { View, StyleSheet, FlatList, Text, Dimensions ,ImageBackground} from "react-native";
+import { View, StyleSheet, FlatList, Text, Dimensions, ImageBackground } from "react-native";
 import MainButton from "./MainButton";
 const DetailsCard = (props) => {
     let containerStyle = styles.container
     if (props.description && props.description.length < 105)
-    containerStyle=styles.containerSM;
-   
+        containerStyle = styles.containerSM;
+
     return (
 
-        
-            <View style={containerStyle}>
-                <View style={styles.TextContainer}>
-                    
-                            <Text style={styles.Title}>{props.title}</Text>
-                            <Text style={styles.Description} >{props.description}</Text>
-                            {/* <Text style={styles.duedate}>{props.dueDate}</Text> */}
-                            <Text style={styles.termsAndCondition}>{props.termsConditions}</Text>
 
-                           
-                        </View>
-                        <MainButton
-                                firstGradient="#1D55C5"
-                                secondGradient="#E93354"
-                                style={styles.chooseClassButton}
-                                onPress={props.onPress}
-                                
-                            >
-                                Choose your class
-                            </MainButton>
-                    
+        <View style={containerStyle}>
+            <View style={styles.TextContainer}>
+
+                <Text style={styles.Title}>{props.title}</Text>
+                <Text style={styles.Description} >{props.description}</Text>
+                {/* <Text style={styles.duedate}>{props.dueDate}</Text> */}
+                <Text style={styles.termsAndCondition}>{props.termsConditions}</Text>
+
+
             </View>
-     
+            <MainButton
+                firstGradient="#1D55C5"
+                secondGradient="#E93354"
+                style={styles.chooseClassButton}
+                onPress={props.onPress}
+
+            >
+                Choose your class
+                            </MainButton>
+
+        </View>
+
 
 
     );
@@ -41,12 +41,12 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FFFFFF',
         width: '100%',
-        height:Dimensions.get('window').height>740? Dimensions.get('window').height*0.40:Dimensions.get('window').height*0.45,
+        height: Dimensions.get('window').height > 740 ? Dimensions.get('window').height * 0.40 : Dimensions.get('window').height * 0.45,
         borderTopLeftRadius: 45,
         borderTopRightRadius: 45,
-        borderBottomLeftRadius: Dimensions.get('window').height>800?50:Dimensions.get('window').height>740?30:20,
-        borderBottomRightRadius:  Dimensions.get('window').height>800?50:Dimensions.get('window').height>740?30:20,
-        marginTop: Dimensions.get('window').height>740?Dimensions.get('window').height*0.60:Dimensions.get('window').height*0.55
+        borderBottomLeftRadius: Dimensions.get('window').height > 800 ? 50 : Dimensions.get('window').height > 740 ? 30 : 20,
+        borderBottomRightRadius: Dimensions.get('window').height > 800 ? 50 : Dimensions.get('window').height > 740 ? 30 : 20,
+        marginTop: Dimensions.get('window').height > 740 ? Dimensions.get('window').height * 0.64 : Dimensions.get('window').height > 640 ? Dimensions.get('window').height * 0.55 : Dimensions.get('window').height * 0.41,
 
     },
     containerSM: {
@@ -55,17 +55,17 @@ const styles = StyleSheet.create({
         backgroundColor: "#FFFFFF",
         width: "100%",
         height:
-          Dimensions.get("window").height > 640 ? Dimensions.get("window").height * 0.4:Dimensions.get("window").height * 0.41 ,
+            Dimensions.get("window").height > 640 ? Dimensions.get("window").height * 0.4 : Dimensions.get("window").height * 0.41,
         borderTopLeftRadius: 45,
         borderTopRightRadius: 45,
-      },
-    
+    },
+
     TextContainer: {
         position: "absolute",
         marginTop: '10%',
         marginLeft: '7%',
         width: '90%',
-         marginRight: '4%',
+        marginRight: '4%',
 
     },
     Title: {
@@ -76,30 +76,30 @@ const styles = StyleSheet.create({
     },
     Description: {
         color: '#AAAAAA',
-        fontSize: Dimensions.get('window').height>600? 14:11,
+        fontSize: Dimensions.get('window').height > 600 ? 14 : 11,
         fontFamily: 'Montserrat',
         marginTop: Dimensions.get('window').height * 0.02,
         marginBottom: Dimensions.get("window").height > 667
-        ? Dimensions.get("window").height * 0.02
-        : Dimensions.get("window").height * 0.02,
+            ? Dimensions.get("window").height * 0.02
+            : Dimensions.get("window").height * 0.02,
     },
     duedate: {
-        fontSize:Dimensions.get('window').height>600? 12:10,
+        fontSize: Dimensions.get('window').height > 600 ? 12 : 10,
         fontFamily: 'Montserrat_SemiBold',
         color: 'black',
-        marginTop:Dimensions.get('window').height>740? Dimensions.get('window').height * 0.14:Dimensions.get('window').height * 0.18,
-        position:'absolute',
+        marginTop: Dimensions.get('window').height > 740 ? Dimensions.get('window').height * 0.14 : Dimensions.get('window').height * 0.18,
+        position: 'absolute',
     },
-    termsAndCondition:{
-        fontSize: Dimensions.get('window').height>600? 12:10,
+    termsAndCondition: {
+        fontSize: Dimensions.get('window').height > 600 ? 12 : 10,
         fontFamily: 'Montserrat_SemiBold',
         color: 'black',
-        marginTop:Dimensions.get('window').height>740? Dimensions.get('window').height * 0.2:Dimensions.get('window').height * 0.23,
-        position:'absolute',
-        flex:1
+        marginTop: Dimensions.get('window').height > 740 ? Dimensions.get('window').height * 0.24 : Dimensions.get('window').height * 0.23,
+        position: 'absolute',
+        flex: 1
     },
     chooseClassButton: {
-        marginTop: Dimensions.get('window').height>740?Dimensions.get('window').height*0.29:Dimensions.get('screen').height*0.31,
+        marginTop: Dimensions.get('window').height > 740 ? Dimensions.get('window').height * 0.31: Dimensions.get('screen').height * 0.31,
         paddingHorizontal: "6%",
 
     }
